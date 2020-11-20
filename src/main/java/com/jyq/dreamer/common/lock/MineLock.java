9c426b141758e432058e8d8cac5f663af071ec4e
jiayuqin <jiayuqin@jd.com>
2020-11-20 10:22:07 +0800

    synchronized自实现

--- src/main/java/com/jyq/dreamer/common/lock/MineLock.java
+++ src/main/java/com/jyq/dreamer/common/lock/MineLock.java
@@ -5,6 +5,7 @@ import sun.misc.Unsafe;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @ClassName: MineClock
@@ -12,9 +13,15 @@ import java.util.concurrent.locks.LockSupport;
  * @author: jiayuqin2
  * @create: 2020-11-17 16:22
  **/
-public class MineClock {
-    private static int state=0;
+public class MineLock {
+    private volatile int state=0;
+    /**
+     * 当前持有锁的线程
+     */
     private Thread holder;
+    /**
+     * 等待队列（公平锁）
+     */
     private ConcurrentLinkedQueue<Thread> queue= new ConcurrentLinkedQueue<>();
 
     public Thread getHolder() {
@@ -25,56 +32,63 @@ public class MineClock {
         this.holder = holder;
     }
 
-    public static int getState() {
+    public int getState() {
         return state;
     }
 
-    public static void setState(int state) {
-        MineClock.state = state;
+    public void setState(int state) {
+        this.state = state;
     }
 
     private  boolean tryAcquire(){
         Thread t=Thread.currentThread();
         if(0==getState()){
-            unsafe.compareAndSwapInt(this,stateOffest,0,1);
-            setHolder(t);
-            return true;
+           if((0==queue.size()||t==queue.peek())&&unsafe.compareAndSwapInt(this,stateOffest,0,1)){
+               setHolder(t);
+               return true;
+           }
+
         }
-        queue.add(t);
         return false;
     }
-    public   boolean lock(){
+    public  void lock(){
         Thread t=Thread.currentThread();
         //1、尝试获取锁 cas-compareAndSwap
         if(tryAcquire()){
-            return true;
+            return;
         }
-        //2、没获取到等待
+        queue.add(t);
+        //2、没获取到自旋等待
         for(;;){
-            if(tryAcquire()){
-                return true;
+            if(t==queue.peek()&&tryAcquire()){
+                queue.poll();
+                return;
             }
             LockSupport.park(t);
         }
         //3、重试获取锁
     }
-    public   boolean unlock(){
+    public  void unlock(){
         Thread t=Thread.currentThread();
         if(t!=getHolder()){
             throw  new RuntimeException(String.format("非当前线程：%s 持有锁，不可释放",t.getName()));
         }
-        unsafe.compareAndSwapInt(this,stateOffest,1,0);
-        holder=null;
-        LockSupport.unpark(queue.peek());
-        return true;
+        if(unsafe.compareAndSwapInt(this,stateOffest,1,0)){
+            setHolder(null);
+            Thread head=queue.peek();
+            if(null!=head){
+                LockSupport.unpark(queue.peek());
+            }
+        }
     }
     private static final Unsafe unsafe=Unsafe.getUnsafe();
     private static  long stateOffest;
     static {
         try {
-            stateOffest = unsafe.objectFieldOffset(MineClock.class.getField("state"));
+            stateOffest = unsafe.objectFieldOffset(MineLock.class.getField("state"));
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
     }
+
 }
